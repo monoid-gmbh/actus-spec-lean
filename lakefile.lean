fbf065eb28188550df119859218119e790884a09
Yves Hauser <yves.hauser@gmail.com>
2026-02-25 11:30:46 +0100

    Setup blueprint

--- lakefile.lean
+++ lakefile.lean
@@ -4,3 +4,9 @@ open Lake DSL
 package «actus» where
 
 lean_lib «Actus» where
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
